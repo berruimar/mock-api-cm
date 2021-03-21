1df6d981352f0143eaa09f061992b86cdce45aa2
Bernabé Ruiz Martínez <47628541+berruimar@users.noreply.github.com>
2021-03-21 18:35:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const fs = require('fs');
 const urlencodedParser = bodyParser.urlencoded({ extended: false, limit: '50mb' })
 const RequestQ = require('express-request-queue');
 const q = new RequestQ();
-
 app.use(morgan('combined'));
-
-
-const delay = require('delay');
 
 
 (async function () {
@@ -20,12 +16,11 @@ const delay = require('delay');
 	}));
 
 	app.post('/login', urlencodedParser, q.run(async (req, res) => {
-		let tmpImgPath = "./tmp/" + Date.now() + "_image.png";
 		if (req.headers.username === "asd" && req.headers.password === "asd") {
-      res.status(200).json({message: "logged in!"};
-    } else {
-      res.status(400).json({message: "invalid credentials"});
-    }
+      			res.status(200).json({message: "logged in!"});
+    		} else {
+      			res.status(400).json({message: "invalid credentials"});
+    		}
 	}));
 
 	app.listen(process.env.PORT || 8000, () => {
